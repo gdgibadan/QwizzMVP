2b6e06209c160c8d757393c6f3d3c90783d3284e
Hamza <hfetuga@gmail.com>
2016-09-20 18:32:41 +0100

    implemented all view methods

--- app/src/main/java/com/ibadan/gdg/qwizzmvp/Home/HomeFragment.java
+++ app/src/main/java/com/ibadan/gdg/qwizzmvp/Home/HomeFragment.java
@@ -1,7 +1,10 @@
 package com.ibadan.gdg.qwizzmvp.Home;
 
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,19 +15,33 @@ import com.ibadan.gdg.qwizzmvp.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
-
+public class HomeFragment extends Fragment implements HomeContract.View {
+    View v;
+    Context context;
+    ProgressDialog progressDialog;
+    public static final String TAG = "HomeFragment3429";
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = getActivity();
+        progressDialog = new ProgressDialog(context);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        v = inflater.inflate(R.layout.fragment_home, container, false);
+        return v;
     }
 
+    @Override
+    public void showProgressIndicator(boolean active) {
+        if (active) { progressDialog.show(); } else { progressDialog.cancel(); }
+    }
 }
--- app/src/main/java/com/ibadan/gdg/qwizzmvp/MainActivity.java
+++ app/src/main/java/com/ibadan/gdg/qwizzmvp/MainActivity.java
@@ -3,11 +3,26 @@ package com.ibadan.gdg.qwizzmvp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ibadan.gdg.qwizzmvp.Home.HomeFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.container) != null){
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            HomeFragment homeFragment = new HomeFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment,
+                    HomeFragment.TAG).commit();
+
+        }
     }
+
+
 }
